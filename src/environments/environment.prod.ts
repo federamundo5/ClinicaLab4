056d2d758e7171596de336ddb18e967927877d22
Federico Ramundo <federicoramundo5@gmail.com>
2020-05-27 16:06:39 -0300

    test

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDpuZbACKhTjadfTVR0frQqIsgmlBvdZh8",
+    authDomain: "clinica-29702.firebaseapp.com",
+    databaseURL: "https://clinica-29702.firebaseio.com",
+    projectId: "clinica-29702",
+    storageBucket: "clinica-29702.appspot.com",
+    messagingSenderId: "865766850495",
+    appId: "1:865766850495:web:6fe5585a8307cbd4742b0a",
+    measurementId: "G-DB4QDJQEGT"
 };
